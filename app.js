ce1b3a156e4fe987945d08610d233586be4ec6c6
Onuchi Johnson <Onuchi.J@bullseye.local>
2023-06-10 12:52:23 +0100

    some lines

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const toggle = document.querySelector(".toggle");
 const cards = document.querySelector(".cards");
 
+toggle.addEventListener("click", () => {
+  cards.classList.toggle("show-cards");
+});
+
 
